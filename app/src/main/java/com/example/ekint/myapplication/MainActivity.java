b54c721a9663ced904d1a467f01ea12b26778af6
Ekin Tiu <ekintiu@gmail.com>
2018-08-03 14:29:02 -0500

    activity_main.xml

--- app/src/main/java/com/example/ekint/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ekint/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.ekint.myapplication;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    android.support.v7.widget.Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+//        ab.setDisplayShowHomeEnabled(true); // show or hide the default home button
+//        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setDisplayShowCustomEnabled(true); // enable overriding the default toolbar layout
+
     }
 }
