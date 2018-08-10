5163bf6e67269b7eec15191531c7811de1ddd71a
Ekin Tiu <ekintiu@gmail.com>
2018-08-10 15:09:11 -0500

    new basic material calendar fragment added

--- app/src/main/java/com/example/ekint/myapplication/SubJournalCalendar.java
+++ app/src/main/java/com/example/ekint/myapplication/SubJournalCalendar.java
@@ -3,38 +3,28 @@ package com.example.ekint.myapplication;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * JournalFragment is the fragment that displays the user's journal and entries.
- * Designed to have three tabs, each to view same data in different way
- * Second in Bottom Navigation View
- * @author ekint
- * @author ayushs
- * @version 1.0
- *
- */
 
-public class JournalFragment extends Fragment {
+/**
+ * Third Tab in Journal Fragment. Experimentation w/ calendar
+ * @author ekint
+ * @version 1.0
+ */
+public class SubJournalCalendar extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    private TabAdapter adapter;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public JournalFragment() {
+    public SubJournalCalendar() {
         // Required empty public constructor
     }
 
@@ -44,11 +34,11 @@ public class JournalFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment JournalFragment.
+     * @return A new instance of fragment SubJournalCalendar.
      */
     // TODO: Rename and change types and number of parameters
-    public static JournalFragment newInstance(String param1, String param2) {
-        JournalFragment fragment = new JournalFragment();
+    public static SubJournalCalendar newInstance(String param1, String param2) {
+        SubJournalCalendar fragment = new SubJournalCalendar();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -69,20 +59,7 @@ public class JournalFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView  = inflater.inflate(R.layout.fragment_journal, container, false);
-        viewPager = (ViewPager) rootView.findViewById(R.id.journalViewPager);
-        tabLayout = (TabLayout) rootView.findViewById(R.id.tabLayout);
-
-//        May be wrong >> check here first for errors
-        adapter = new TabAdapter(getChildFragmentManager());
-        adapter.addFragment(new SubJournalRecent(), "Recent");
-        adapter.addFragment(new SubJournalTimeline(), "Timeline");
-        adapter.addFragment(new SubJournalCalendar(), "Calendar");
-
-        viewPager.setAdapter(adapter);
-        tabLayout.setupWithViewPager(viewPager);
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_sub_journal_calendar, container, false);
     }
 
 }
